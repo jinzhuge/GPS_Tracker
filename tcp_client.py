eb63eda8aa67998962d383fdb448b55336959778
jinzhuge <52116801+jinzhuge@users.noreply.github.com>
2019-07-19 16:32:18 +0800

    all

--- tcp_client.py
+++ tcp_client.py
@@ -6,15 +6,13 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
 # server_address = ('18.221.174.251', 8080)
-server_address = ('18.221.174.251', 8080)
+server_address = ('192.168.1.4', 8080)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
 try:
-
-    # Send Gps data
-    # message = b'This is our message. It is very long but will only be transmitted in chunks of 16 at a time'
-    message = '{ "name":"John", "age":30, "city":"New York"}'
+    # gps data
+    message = '!1,865472032140613;'
     if sys.version_info < (3, 0):
         data = bytes(message)
     else:
@@ -28,9 +26,9 @@ try:
     amount_expected = len(message)
 
     while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print('received {!r}'.format(data))
+        recvData = sock.recv(16)
+        amount_received += len(recvData)
+        print(str(recvData))
 
 finally:
     print('closing socket')
--- tcp_server.py
+++ tcp_server.py
@@ -1,12 +1,76 @@
-
 import socket
-import json
 import datetime
 import pymongo
 
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-db = myclient['GPS_database']
-collection = db['GPS_info']
+
+def convertRecvData(str):
+
+    return;
+
+
+def checkTypeData(str):
+    result = [x.strip() for x in str.split(',')]
+    insert_data = {}
+    if result[0] == '!1':
+        insert_data = {
+            "Protocol": result[0],
+            "GPS_imei": result[1]
+        }
+    elif result[0] == '!D':
+        insert_data = {
+            "Protocol": result[0],
+            "Date": result[1],
+            "Time": result[2],
+            "Latitude": result[3],
+            "Longitude": result[4],
+            "Speed": result[5],
+            "Direction": result[6],
+            "EventCode": result[7],
+            "Altitude": result[8],
+            "Battery": result[9],
+            "SatelliteUse": result[10],
+            "SatelliteNum": result[11],
+            "Reserved": result[12]
+        }
+    elif result[0] == '!3':
+        insert_data = {
+            "Protocol": result[0],
+            "response": result[1]
+        }
+    elif result[0] == '!4':
+        insert_data = {
+            "Protocol": result[0],
+            "responseFunction": result[1]
+        }
+    elif result[0] == '!7':
+        insert_data = {
+            "Protocol": result[0],
+            "firmwre_version": result[1],
+            "signal_strength": result[2]
+        }
+    elif result[0] == '!5':
+        insert_data = {
+            "Protocol": result[0],
+            "CSQ":result[1],
+            "GPS_location": result[2]
+        }
+    manageDatabase(insert_data)
+    return;
+
+
+def printme(str):
+    print(str)
+    return ;
+
+
+def manageDatabase(insert_data):
+
+    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+    db = myclient['GPS_database']
+    collection = db['GPS_info']
+
+    x = collection.insert_one(insert_data)
+    return;
 
 
 # Create a TCP/IP socket
@@ -14,9 +78,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
 server_address = ('0.0.0.0', 8080)
-# print('Starting up on {} port {}'.format(*server_address))
+print('Starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
-
 # Listen for incoming gps device connections
 sock.listen(5)
 
@@ -25,34 +88,20 @@ while True:
     print('waiting for a Gps Device connection')
     connection, client_address = sock.accept()
     try:
-        # print('connection from', client_address)
-        i=0;
+        print('connection from', client_address)
+        i = 0
         # Receive the data in small chunks and retransmit it
         while True:
-            data = connection.recv(4096)
-
+            recvDataFromClient = connection.recv(4096)
             # data = json.loads(response_data.decode("utf-8"))
             # data = json.loads(resp_parsed)
 
-
-            if data:
-                # print('sending data back to the client')
-                #
-                # if sys.version_info < (3, 0):
-                #     data = bytes(data)
-                # else:
-                #     data = bytes(data, 'utf8')
-
-                connection.sendall(data)
-                data = str(data)
-                print(data)
-                i = i+1
-                # insert gps data in MongoDB
-                post = {"id": i,
-                        "gps_info": data,
-                        "date": datetime.datetime.utcnow()}
-
-                x = collection.insert_one(post)
+            if recvDataFromClient:
+                connection.sendall(recvDataFromClient)
+                recvDataString = recvDataFromClient.decode("utf-8")
+                print(recvDataString)
+                # recvDataString = str(recvDataFromClient, 'utf-8')
+                checkTypeData(recvDataString)
 
             else:
                 # print('no data from', client_address)
@@ -60,5 +109,6 @@ while True:
 
     finally:
         # Clean up the connection
-        # print("Closing current connection")
+        print("Closing current connection")
         connection.close()
+
